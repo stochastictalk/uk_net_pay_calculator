dee71346bc70a9df8ddeb455b7161c6e3288544e
Jerome <jerome.wynne18@ic.ac.uk>
2020-10-24 12:42:26 +0100

    Define yearly_summary

--- uk_net_pay_calculator.py
+++ uk_net_pay_calculator.py
@@ -4,43 +4,65 @@
 # Author: Jerome Wynne
 # Summary: a function for computing monthly net pay over one financial year.
 
-import numpy as np
+def yearly_summary(gross_annual_salary:float, to_pension:float):
 
-def uk_net_pay_calculator(gross_annual_salary:float):
-    # For 2021
-    # Tax year runs from 6 April to 5 April
-    # Personal allowance of £12,500
-    #   For the first £12,500 no tax is paid
-    #   Only earnings above this sum are taxed
-    # NI threshold is £8,632
-    #
-    # Income tax
-    # [0, 12500]: 0
-    # (12500, 50000]: 0.2
-    # (50000, 150000]: 0.4
-    # (150000, inf): 0.45
+    ni = ni_contribution(gross_annual_salary)
+    plan2 = plan_2_contribution(gross_annual_salary)
+    postgrad = postgraduate_contribution(gross_annual_salary)
+    pen_con = pension_contribution(gross_annual_salary, to_pension)
+    salary_less_pension = gross_annual_salary - pen_con
+    tax = income_tax(salary_less_pension)
 
-    # National insurance
-    # Employees pay Class National Insurance Contributions
-    # You pay NI contributions if you earn more than £183 a week
-    # 12% of earnings > £183 and <= £962 a week
-    # 2% of earnings > £962
-    pass
+    summary_dct = {
+        'Gross annual salary': gross_annual_salary,
+        'Pension': pen_con,
+        'Tax': tax,
+        'National Insurance': ni,
+        'Plan 2 loan': plan2,
+        'Postgraduate loan': postgrad,
+        'Net annual salary': gross_annual_salary - sum([pen_con, tax,
+                                                        ni, plan2, postgrad])
+    }
+    return summary_dct
 
-def get_income_tax(gross_annual_salary: float):
+    return (salary_less_pension -
+            income_tax(salary_less_pension) -
+            ni_contribution(salary_less_pension) -
+            plan_2_contribution(salary_less_pension) -
+            postgraduate_contribution(salary_less_pension))
+
+def income_tax(gross_annual_salary: float):
+    # ref: https://www.moneyadviceservice.org.uk/en/articles/tax-and-national-insurance-deductions
     tax_bands = [(0, 12500, 0.),
                  (12500, 50000, 0.2),
                  (50000, 150000, 0.4),
-                 (150000, np.inf, 0.45)] # 2020/21 income tax bands
-    return(get_marginal_tax(gross_annual_salary, tax_bands))
+                 (150000, float('inf'), 0.45)] # 2020/21 income tax bands
+    return(marginal_tax(gross_annual_salary, tax_bands))
 
-def get_ni_contribution(gross_annual_salary: float):
+def ni_contribution(gross_annual_salary: float):
+    # ref: https://www.moneyadviceservice.org.uk/en/articles/tax-and-national-insurance-deductions
     ni_bands = [(0, 183.*52, 0.),
                 (183.*52, 962.*52, 0.12),
-                (962.*52, np.inf, 0.02)] # 2020/21 NI contribution bands
-    return(get_marginal_tax(gross_annual_salary, ni_bands))
+                (962.*52, float('inf'), 0.02)] # 2020/21 NI contribution bands
+    return(marginal_tax(gross_annual_salary, ni_bands))
 
-def get_marginal_tax(gross_annual_salary: float, tax_bands: list):
+def marginal_tax(gross_annual_salary: float, tax_bands: list):
     gas = gross_annual_salary
     tax_by_band = [b[2]*(max(b[0], min(gas, b[1])) - b[0]) for b in tax_bands]
     return(sum(tax_by_band))
+
+def pension_contribution(gross_annual_salary: float,
+                                        frac_contributed:float):
+    return(gross_annual_salary*frac_contributed)
+
+def plan_2_contribution(gross_annual_salary: float):
+    # ref: https://www.gov.uk/repaying-your-student-loan/what-you-pay
+    annual_threshold = 2214.*12
+    plan_2_rate = 0.09
+    return(max(0., gross_annual_salary - annual_threshold)*plan_2_rate)
+
+def postgraduate_contribution(gross_annual_salary: float):
+    # ref: https://www.gov.uk/repaying-your-student-loan/what-you-pay
+    annual_threshold = 1750.*12
+    postgraduate_rate = 0.06
+    return(max(0., gross_annual_salary - annual_threshold)*postgraduate_rate)
