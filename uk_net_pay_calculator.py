8b379bc20bce334705000c99674101c4ea3e6a31
Jerome <jerome.wynne18@ic.ac.uk>
2020-10-24 10:29:38 +0100

    Initialized calculator script

---
+++ uk_net_pay_calculator.py
@@ -0,0 +1,8 @@
+# -*- utf-8 -*-
+
+# Created: 24th October 2020
+# Author: Jerome Wynne
+# Summary: a function for computing monthly net pay over one financial year.
+
+def uk_net_pay_calculator():
+    pass
